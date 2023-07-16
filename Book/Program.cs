fd8087ec22247b21912a1e57ce7cc0d574a00953
pratibha <pratibha1111kokil@gmail.com>
2023-07-16 13:07:25 +0530

    My Lab

---
+++ Book/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Book
+
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book();
+
+            Console.WriteLine("Information of book 1:");
+            Console.Write("Input name of the book: ");
+            book1.Name = Console.ReadLine();
+            Console.Write("Input the author: ");
+            book1.Author = Console.ReadLine();
+
+            Console.WriteLine("\nInformation of book 2:");
+            Console.Write("Input name of the book: ");
+            book2.Name = Console.ReadLine();
+            Console.Write("Input the author: ");
+            book2.Author = Console.ReadLine();
+
+            Console.WriteLine("\nInserted information of the books:");
+            Console.WriteLine("Book 1: Name = {0}, Author = {1}", book1.Name, book1.Author);
+            Console.WriteLine("Book 2: Name = {0}, Author = {1}", book2.Name, book2.Author);
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
---
+++ SpeedCal/SpeedCal/Controllers/SpeedController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedCal.Controllers
+{
+    public class SpeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Convert(double speed)
+        {
+            double speedInKmPerHour = speed * 3.6;
+            ViewBag.SpeedInKmPerHour = speedInKmPerHour;
+            return View("Result");
+        }
+    }
+}
